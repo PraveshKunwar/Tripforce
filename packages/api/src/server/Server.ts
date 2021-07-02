4d00fd848d4fc41b5e677cc550fae92bd8189214
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2021-07-01 20:57:23 -0700

    refactor(Server): basic functions & server set up

--- packages/api/src/server/Server.ts
+++ packages/api/src/server/Server.ts
@@ -1,5 +1,35 @@
-import { Application, Request, Response, NextFunction } from "express";
+import express, { Application } from "express";
+import dotenv from "dotenv";
 
 export class Server {
-  public start() {}
+  public app: Application;
+
+  public constructor() {
+    this.app = express();
+    this.registerMiddlewares();
+    this.registerRoutes();
+
+    dotenv.config();
+  }
+
+  public start() {
+    this.app.listen(process.env.TRIPFORCE_API_PORT, () => {
+      console.log(`Server started on port ${process.env.TRIPFORCE_API_PORT}`);
+    });
+  }
+
+  private registerMiddlewares() {
+    this.app.use(express.json());
+    this.app.use(async (req, res, next) => {
+      console.log(`[${req.method} - ${req.path}]`);
+
+      next();
+    });
+  }
+
+  private registerRoutes() {
+    this.app.get("/", (req, res) => {
+      res.json({ success: true, message: "Tripforce API" });
+    });
+  }
 }
